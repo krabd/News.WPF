b26c9eedb708adae250a5a4b2dc29aa605607013
krab <dimaandrievskiy@mail.ru>
2020-04-07 00:16:54 +0300

    news view

---
+++ News/ViewModels/Items/NewsItemViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using News.CoreModule.ViewModels;
+using News.Domain.Models;
+
+namespace News.ViewModels.Items
+{
+    public class NewsItemViewModel : ViewModelBase
+    {
+        private string _author;
+        private string _title;
+        private string _description;
+        private DateTime _publishedDate;
+
+        public string Author
+        {
+            get => _author;
+            set => SetProperty(ref _author, value);
+        }
+
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        public string Description
+        {
+            get => _description;
+            set => SetProperty(ref _description, value);
+        }
+
+        public DateTime PublishedDate
+        {
+            get => _publishedDate;
+            set => SetProperty(ref _publishedDate, value);
+        }
+
+        public void Initialize(NewsModel news)
+        {
+            Author = news.Author;
+            Title = news.Title;
+            Description = news.Description;
+            PublishedDate = news.PublishedDate;
+        }
+    }
+}
--- News/ViewModels/NewsViewModel.cs
+++ News/ViewModels/NewsViewModel.cs
@@ -1,13 +1,18 @@
-﻿using System.Threading;
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using News.CoreModule.ViewModels;
 using News.DataAccess.Interfaces;
+using News.ViewModels.Items;
 
 namespace News.ViewModels
 {
     public class NewsViewModel : ViewModelBase
     {
         private readonly INewsRepository _newsRepository;
+
+        public ObservableCollection<NewsItemViewModel> News { get; } = new ObservableCollection<NewsItemViewModel>();
 
         public NewsViewModel(INewsRepository newsRepository)
         {
@@ -19,6 +24,13 @@ namespace News.ViewModels
             var news = await _newsRepository.GetNewsAsync(token);
             token.ThrowIfCancellationRequested();
 
+            News.Clear();
+            News.AddRange(news.Select(i =>
+            {
+                var newsItem = new NewsItemViewModel();
+                newsItem.Initialize(i);
+                return newsItem;
+            }));
         }
     }
 }
