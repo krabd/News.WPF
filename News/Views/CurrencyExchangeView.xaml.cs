025229d5fc18725a5e551edb00648e9dc5a052bd
krab <dimaandrievskiy@mail.ru>
2020-04-08 00:24:09 +0300

    replace datatemplates with usercontrols

---
+++ News/Views/CurrencyExchangeView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace News.Views
+{
+    /// <summary>
+    /// Interaction logic for CurrencyExchangeView.xaml
+    /// </summary>
+    public partial class CurrencyExchangeView : UserControl
+    {
+        public CurrencyExchangeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ News/Views/NewsView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace News.Views
+{
+    /// <summary>
+    /// Interaction logic for NewsView.xaml
+    /// </summary>
+    public partial class NewsView : UserControl
+    {
+        public NewsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
